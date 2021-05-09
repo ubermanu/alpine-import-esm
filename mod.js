8f9b7859aea2fb0f5784133b1f644ffa5634aeba
ubermanu <manu@paca.happy-dev.fr>
2021-05-09 12:45:09 +0200

    fix first paint issue remove types

--- mod.js
+++ mod.js
@@ -1,15 +1,24 @@
 /**
+ * Import a module into your component x-data.
+ * Only the default export is currently supported.
+ *
+ * @example
+ *  <div x-data="$import('./example.mjs')"></div>
+ *
  * @param {string} path
  * @return {{}}
  */
-export default function $import(path) {
-  let data = {}
+Alpine.addMagicProperty('import', (el) => {
+  return (path) => {
+    let data = {}
 
-  import(path)
-    .then((module) => {
-      Object.assign(data, module.default ?? {})
-    })
-    .catch((e) => console.warn(e))
+    import(path)
+      .then((module) => {
+        Object.assign(data, module.default ?? {})
+        el.__x?.updateElements(el)
+      })
+      .catch((e) => console.warn(e))
 
-  return data
-}
+    return data
+  }
+})
